e400b4761df4279c3d9025b0edc9f28d88f198b1
Peshava-Afshordel <76878498+Peshava-Afshordel@users.noreply.github.com>
2024-10-25 12:54:03 +0330

    Delete Script/app.js

--- Script/app.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector(".toggle__menu");
-const menu = document.querySelector(".mobile__menu");
-
-menuBtn.addEventListener("click", function () {
-  menu.classList.toggle("menu--active");
-});
