5bc69286133b144448935858f965ce64041c799a
Peshava-Afshordel <76878498+Peshava-Afshordel@users.noreply.github.com>
2024-10-25 13:03:29 +0330

    Update app.js

--- Script/app.js
+++ Script/app.js
@@ -1,1 +1,17 @@
+const menuBtn = document.querySelector(".toggle__menu");
+const menu = document.querySelector(".mobile__menu");
+
+const linkTag = document.querySelectorAll("a");
+
+linkTag.forEach((a) => {
+  a.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+});
+
+menuBtn.addEventListener("click", function () {
+  menu.classList.toggle("menu--active");
+
+  document.querySelector("body").classList.toggle("hidden");
+});
 
